a22d511ddbb3307babb8c7a540191955bc3a2667
Mayu <mayuran.ganesathas@gmail.com>
2023-02-09 18:57:10 -0500

    mg-fetching

--- components/MapChart.tsx
+++ components/MapChart.tsx
@@ -5,6 +5,7 @@ import {
   Geography,
   ZoomableGroup,
   Marker,
+  Annotation,
 } from "react-simple-maps";
 
 //https://www.react-simple-maps.io/examples/zoom-pan/
@@ -12,11 +13,21 @@ import {
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/north-america.json";
 
-const MapChart = () => {
+export interface MapChartProps {
+  setSelectedLocation: (e: string) => void;
+}
+
+export const MapChartWrapper = ({ setSelectedLocation }: MapChartProps) => {
   return (
     <div>
       <ComposableMap projection="geoMercator">
-        <ZoomableGroup center={[0, 0]} zoom={9}>
+        <ZoomableGroup
+          center={[-82.862, 46.844]}
+          zoom={4}
+          onMoveStart={({ coordinates, zoom }) => {
+            console.log(coordinates, zoom);
+          }}
+        >
           <Geographies geography={geoUrl}>
             {({ geographies }) =>
               geographies.map((geo) => (
@@ -24,8 +35,56 @@ const MapChart = () => {
               ))
             }
           </Geographies>
-          <Marker coordinates={[0, 0]}>
-            <circle r={3} fill="#FF5533" />
+          <Annotation
+            subject={[-78.667, 43.8944]}
+            dx={-20}
+            dy={-30}
+            connectorProps={{
+              stroke: "#FF5533",
+              strokeWidth: 0.25,
+              strokeLinecap: "round",
+            }}
+          >
+            <text
+              className="cursor-pointer"
+              onClick={() => setSelectedLocation("Toronto")}
+              x="-1"
+              textAnchor="end"
+              alignmentBaseline="middle"
+              fill="#F53"
+              fontSize={5}
+            >
+              {"Toronto"}
+            </text>
+          </Annotation>
+          <Marker coordinates={[-78.667, 43.8944]}>
+            <circle r={1} fill="#FF5533" />
+          </Marker>
+
+          <Annotation
+            subject={[-79.667, 43.1944]}
+            dx={-20}
+            dy={-10}
+            connectorProps={{
+              stroke: "#FF5533",
+              strokeWidth: 0.25,
+              strokeLinecap: "round",
+            }}
+          >
+            <text
+              className="cursor-pointer"
+              x="-1"
+              textAnchor="end"
+              alignmentBaseline="middle"
+              fill="#F53"
+              fontSize={5}
+              onClick={() => setSelectedLocation("Hamilton")}
+            >
+              {"Hamilton"}
+            </text>
+          </Annotation>
+          <Marker coordinates={[-79.667, 43.1944]}>
+            <circle r={1} fill="#FF5533" />
           </Marker>
         </ZoomableGroup>
       </ComposableMap>
@@ -33,4 +92,4 @@ const MapChart = () => {
   );
 };
 
-export default MapChart;
+export default MapChartWrapper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import MapChart from "../components/MapChart";
+import MapChart, { MapChartWrapper } from "../components/MapChart";
 import { useState } from "react";
 import { GetServerSideProps } from "next";
 import prisma from "../lib/prisma";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home(props: any) {
   const [data, setData] = useState();
+  const [selectedLocation, setSelectedLocation] = useState("");
+
+  const locations = [
+    "Toronto",
+    "Hamilton",
+    "Additional Locations Coming Soon!",
+  ];
+
   const fetchData = async (e: React.SyntheticEvent) => {
     fetch("api/QUERY_TABLE")
       .then((data) => {
@@ -22,29 +28,43 @@ export default function Home(props: any) {
       });
   };
 
-  let test = data;
   return (
-    <div className="text-blue-600">
-      <div>Select A Location</div>
-      <div className="border-2 w-8/12">
-        <MapChart />
+    <div className="">
+      <div className={`flex-nowrap rounded text-sm `}>
+        <div className="border-2 w-8/12">
+          <MapChartWrapper setSelectedLocation={setSelectedLocation} />
+        </div>
+        <div className="px-2 flex-nowrap">
+          <div className="">Location Selected:</div>
+          <select
+            className={`rounded bg-gray-300 py-1 w-6/12`}
+            name="Select From List"
+            id="Select From List"
+            value={selectedLocation}
+            onChange={(e) => setSelectedLocation(e.target.value)}
+          >
+            {locations.map((location) => (
+              <option value={location}> {location} </option>
+            ))}
+          </select>
+          <div className="py-2">
+            {" "}
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex"
+              onClick={fetchData}
+            >
+              {" "}
+              Fetch Latest Data
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div>Table Content Here</div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={fetchData}
-      >
-        {" "}
-        Test
-      </button>
-      {data && <div>{JSON.stringify(props)}</div>}
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.pollen_data.findMany({});
+  const feed = await prisma.pollen_data.findMany({ take: 10 });
   return {
     props: { feed },
   };
