f4cc4acd0ca8031d3445a4b686a7a65242f8d788
Mayu <mayuran.ganesathas@gmail.com>
2023-02-10 13:01:48 -0500

    graph color change

--- components/GraphChart.tsx
+++ components/GraphChart.tsx
@@ -98,7 +98,7 @@ export const GraphChart = ({ dataLocation, graphData }: GraphChartProps) => {
       {
         label: `Number of Pollen Particles`,
         data: [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12],
-        backgroundColor: "rgba(60, 122, 237, 0.5)",
+        backgroundColor: "rgba(60, 122, 237)",
       },
     ],
   };
