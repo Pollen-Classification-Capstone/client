799530ada95e232b2bb3663c6c674617786afdb3
Mayu <mayuran.ganesathas@gmail.com>
2023-02-10 12:09:08 -0500

    mgresizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home(props: any, result: any) {
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="w-10/12">
           <GraphChart
             dataLocation={selectedLocation}
             graphData={filteredDataObject}
