6b1ea65c52dcd38a348bd81e28c7d8290f528d01
Mayu <mayuran.ganesathas@gmail.com>
2023-04-23 17:46:34 -0400

    mg

--- components/SectionOne.tsx
+++ components/SectionOne.tsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 export const LandingPage = ({}) => {
   return (
     <div>
-      <div className="bg-black py-20 left-full px-20 justify-center relative flex ">
+      <div className="bg-black py-20 left-full px-20 justify-center relative flex  ">
         <Image
           src="/images/spring-pollen-allergies.jpg"
           width={600}
@@ -24,21 +24,21 @@ export const LandingPage = ({}) => {
           className="  "
         />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center relative ">
         <div>
-          <p className="text-white  absolute bottom-80 left-44 w-3/12">
+          <p className="text-white  absolute bottom-96 left-44">
             Hey there, do you know what pollen is? It's those tiny things that
             plants make to help them reproduce. But sometimes scientists want to
             know what kind of pollen it is and it's hard to tell just by looking
             at it.
           </p>
         </div>
-        <div className="text-white absolute bottom-44  left-44 w-3/12">
+        <div className="text-white absolute bottom-64 left-44">
           That's where our advanced pollen classification algorithm comes in! It
           can look at pictures of the pollen and figure out what kind it is all
           by itself. You just need to give it the pictures and it does the rest.
         </div>
-        <div className="text-white absolute bottom-10  left-44 w-3/12">
+        <div className="text-white absolute bottom-32  left-44">
           This algorithm can help identify and measure pollen levels in
           different areas around the world and help give accurate up to date
           measurements. See for yourself with our live demo!
